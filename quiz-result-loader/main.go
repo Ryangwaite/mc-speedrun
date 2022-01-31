9f04f13e67c7d78fa09d28e550baf76e89e66104
Ryan Garthwaite <ryangwaite@gmail.com>
2022-01-31 20:49:12 +1000

    quiz-result-loader: Added merging of redis loaded data and uploaded question file

--- quiz-result-loader/main.go
+++ quiz-result-loader/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"strconv"
 
 	extract "github.com/Ryangwaite/mc-speedrun/quiz-result-loader/extract"
+	"github.com/Ryangwaite/mc-speedrun/quiz-result-loader/quiz"
 )
 
+func combineExtractedQuizAndQuestions(extractedQuiz quiz.Quiz, questions quiz.QuestionAndAnswers) (quiz.Quiz, error) {
+	for i, question := range extractedQuiz.Questions {
+		// The loader didn't know the question text so just substituted the question index instead
+		qIndex, err := strconv.Atoi(question.Question)
+		if err != nil {
+			return extractedQuiz, errors.New("expected question from extracted quiz to be an index")
+		}
+		// Copy the question fields into the quiz
+		qAndA := questions[qIndex]
+		extractedQuiz.Questions[i].Question = qAndA.Question
+		extractedQuiz.Questions[i].Options = qAndA.Options
+		extractedQuiz.Questions[i].CorrectOptions = qAndA.Answers
+	}
+	return extractedQuiz, nil
+}
 
 func main() {
 	var ctx = context.Background()
+
+	//// Extract ////
 
 	extractor := extract.NewRedisExtractor(extract.RedisExtractorOptions{
 		Addr: "localhost:6379",
@@ -18,18 +38,24 @@ func main() {
 
 	quizId := "sampledata1"
 
-	// questionSetPath := "/tmp/question-sets/" + quizId + ".json"
-	// questions, err := quiz.LoadQuestionsFromFile(questionSetPath)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	questionSetPath := "/tmp/question-sets/" + quizId + ".json"
+	questions, err := quiz.LoadQuestionsFromFile(questionSetPath)
+	if err != nil {
+		panic(err)
+	}
 
-	quiz, err := extractor.Extract(ctx, quizId)
+	// NOTE: This extracted quiz doesn't have completed questions
+	extractedQuiz, err := extractor.Extract(ctx, quizId)
 	if err != nil {
 		panic("Failed to load: " + err.Error())
 	}
 
-	// TODO: Join questions into quiz
+	completeQuiz, err := combineExtractedQuizAndQuestions(extractedQuiz, questions)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Complete loaded quiz: %+v\n", completeQuiz)
 
-	fmt.Printf("Loaded quiz: %+v\n", quiz)
+	//// Load ////
+
 }
