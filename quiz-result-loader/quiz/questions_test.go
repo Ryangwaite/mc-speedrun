a32159da7c973d83176518c570ba57104a371f92
Ryan Garthwaite <ryangwaite@gmail.com>
2022-06-05 09:25:04 +1000

    quiz-result-loader: Added test for questions

---
+++ quiz-result-loader/quiz/questions_test.go
@@ -0,0 +1,39 @@
+package quiz
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestQuizFileFromBytes(t *testing.T) {
+	qAndA := QuestionAndAnswers{
+		{
+			Question: "question 1",
+			Category: "food",
+			Options: []string {"a", "b", "c", "d"},
+			Answers: []int{1, 2},
+		},
+		{
+			Question: "question 2",
+			Category: "tech",
+			Options: []string {"i", "ii", "iii", "iv"},
+			Answers: []int{0, 2, 3},
+		},
+	}
+
+	qAndABytes, err := json.Marshal(qAndA)
+	if err != nil {
+		t.Fatalf("Failed to serialize QuestionAndAnswer: %v", err)
+	}
+
+	deserializedQAndA, err := QuizFileFromBytes(&qAndABytes)
+	if err != nil {
+		t.Fatalf("Failed to deserialize QuestionAndAnswers bytes: %v", err)
+	}
+
+	if diff := cmp.Diff(qAndA, deserializedQAndA); diff != "" {
+		t.Fatalf("Deserialized bytes don't match: %s", diff)
+	}
+}
