35c972db3e597f237034e54e7e6f0319a4ec80ff
Ryan Garthwaite <ryangwaite@gmail.com>
2022-08-20 17:57:29 +1000

    deployment/aws: Deployed static assets to s3

--- deployment/aws/infra/lib/infra-stack.ts
+++ deployment/aws/infra/lib/infra-stack.ts
@@ -7,6 +7,8 @@ import * as elbtargets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets'
 import * as efs from 'aws-cdk-lib/aws-efs'
 import * as ecs from 'aws-cdk-lib/aws-ecs'
 import * as sqs from 'aws-cdk-lib/aws-sqs'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment'
 import { Elasticache } from './elasticache';
 
 const QUESTION_SET_LOADER_ENV_VAR_PREFIX = "MC_SPEEDRUN_"
@@ -286,6 +288,19 @@ export class InfraStack extends Stack {
       exportName: "ALB-DNS-name",
       description: "The public DNS name to reach the ALB",
       value: alb.loadBalancerDnsName,
+    })
+
+    const frontendBucket = new s3.Bucket(this, "FrontendBucket", {
+      autoDeleteObjects: true,  // just to make cleanup easier, wouldn't want this for prod
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true, // TODO: Disable once cloudfront is in use
+    })
+    new s3deploy.BucketDeployment(this, "DeployFrontend", {
+      sources: [
+        s3deploy.Source.asset("../../../ui/build"), // NOTE: The sources must exist for this to work
+      ],
+      destinationBucket: frontendBucket,
     })
   }
 }
